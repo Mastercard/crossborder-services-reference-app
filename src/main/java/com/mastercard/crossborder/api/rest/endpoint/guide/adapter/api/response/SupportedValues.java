cb9f0149cbd4696b9e6362e07ae392ccdb97aad9
snehalubale <108735539+snehalubale@users.noreply.github.com>
2025-12-24 14:22:47 +0530

    Create SupportedValues.java

---
+++ src/main/java/com/mastercard/crossborder/api/rest/endpoint/guide/adapter/api/response/SupportedValues.java
@@ -0,0 +1,12 @@
+package com.mastercard.crossborder.api.rest.endpoint.guide.adapter.api.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class SupportedValues {
+
+    private String id;
+    private String name;
+}
