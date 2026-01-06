a9b8dfefa9714ee56bb6437c8ec206b498a33f54
snehalubale <108735539+snehalubale@users.noreply.github.com>
2026-01-06 11:54:49 +0530

    Update MastercardHttpHeaders.java

--- src/main/java/com/mastercard/crossborder/api/constants/MastercardHttpHeaders.java
+++ src/main/java/com/mastercard/crossborder/api/constants/MastercardHttpHeaders.java
@@ -2,7 +2,9 @@ package com.mastercard.crossborder.api.constants;
 
 public enum MastercardHttpHeaders  {
     ENCRYPTED_HEADER("x-encrypted"),
-    PARTNER_REF_ID("Partner-Ref-Id");
+    PARTNER_REF_ID("Partner-Ref-Id"),
+    SPECIFICATION_TYPE("Specification-Type");
+
     private final String text;
 
     MastercardHttpHeaders(String text) {
